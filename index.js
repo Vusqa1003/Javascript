47550d507b4c2dab281105c06652eb9811c94059
Vusqa1003 <vusqarafique@gmail.com>
2021-03-29 22:55:36 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Thanks for your input");
+console.log("Hello World");
+console.log(2+8);
+document.write("Hello World");
+document.write(2+8);
+var age = 25;
+var weight = 150.5;
+var newweight = weight + 20;
+console.log(newweight);
