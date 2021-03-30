605cb524a832ae9830ec7f69bd5daf2d76bb7518
Vusqa1003 <vusqarafique@gmail.com>
2021-03-31 00:21:16 +0500

    session 1

---
+++ session 1/index.js
@@ -0,0 +1,169 @@
+/*alert("Thanks for your input");
+console.log("Hello World");
+console.log(2+8);
+document.write("Hello World");
+document.write(2+8);
+var age = 25;
+var weight = 150.5;
+var newweight = weight + 20;
+console.log(newweight);
+var a = "Hello";
+typeof a;
+var a=2;
+var b=5;
+var c=0;
+c=a+b;
+alert(a);
+console.log(c)
+
+var a = 5
+a*4
+alert(a)
+console.log(a)*/
+
+
+/*var rose= "hello";
+var Rose= "world";
+console.log(rose);
+console.log(Rose);
+//console.log(ROSE); //Error due to case sensitive
+var a= 5
+var b= 3
+var c= a+b;     //addition
+var d= a-b;     //subtraction
+var e= a*b;     //multiplication
+var f= a/b;      //division
+var g= a%b;      //modulus
+var h= a**b;       //exponential
+console.log(c);     
+console.log(d);     
+console.log(e);     
+console.log(f);    
+console.log(g);    
+console.log(h); 
+  
+var ass=5;
+ass+=5;
+console.log(ass);
+
+var exp = 5+2*3-2/2;
+document.write(exp);
+document.write("<br>")
+
+var a = 3 + 5 * 2; 
+var b = 8 / 2 - 1; 
+var c = 3 % 2 + 4 - 1; 
+var d = a + 5 * c - b / (3 + b);
+document.write(d)
+document.write("<br>")*/
+
+/*var  inc= 12;
+inc += 1;
+inc -= 1;
+document.write(inc);
+
+
+var prefixAge = 12;
+++prefixAge;
+alert(prefixAge);
+--prefixAge;
+alert(prefixAge); 
+document.write("<br>")
+
+var postfixAge = 12;
+postfixAge++;
+document.write(postfixAge);
+document.write("<br>"); 
+postfixAge--;
+document.write(postfixAge); 
+document.write("<br>")
+// postfix
+var p=12;
+var postp= p--;
+//document.write(p);
+document.write(postp);
+
+// increment & decrement 
+var a = 4;
+var b = 3;
+var c = a++ + --b - --a;
+alert(a); 
+alert(b);  
+alert(c);  
+
+// concatenating
+var fname= "Vusqa";
+var lname= "Rafique";
+var fullName=fname+" "+lname;
+document.write(fullName);*/
+
+//prompt
+/*var question = "What is your name?";
+var defaultAnswer = "Vusqa";
+var name = prompt(question, defaultAnswer);
+console.log("Name = "+ name);
+
+
+//concatenate the answer given in prompt with the newage
+var question = "What is your age?";
+var age = prompt(question);
+var newAge = age + 5; // It will concatenate
+console.log("New Age = "+ newAge);*/
+
+//convert string to number
+/*var value1 = "3"; //in string number is given
+var value2 = "5";
+var value3 = value1+value2;
+console.log(value3);
+
+//prompt provide string even if we given number then we convert string to number by using parseInt
+//now it will not concatenate the number but add the two numbers
+var age = prompt("What is your age");
+var num = 4;
+var sum = parseInt(age) + num;
+console.log(sum);
+
+var a = Number(true); //returns 1
+var b = Number(false); //returns 0
+var c = Number("999"); //returns 999
+var d = Number("999 888"); //returns NAN because there is space btw 2 numbers
+var e = Number("Hello"); //returns NAN bcz given value is string
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+
+
+//comparison operator
+var a = 3;
+// == only compare value
+console.log(a == 3); //return true
+console.log(a == "3"); //return true 
+console.log(a == 6); //return false
+//(===)compare both the value and data type if any one of them is false then answer is false
+console.log(a === 3); //return true
+console.log(a === "3"); //return false bcz the the datatype is string
+console.log(a === 6); //return false
+
+
+// logical operators(and, or, not)
+// AND operator false when any of the value from both is false
+var a = 40;
+var b = a > 20 && a < 70;
+alert(b);
+
+// OR operator false when both values are false
+var a = 40;
+var b = a > 20 || a < 10;
+alert(b);
+
+//Not operator 
+var c = 80;
+var d = !(c > 50);
+alert(d);
+
+//double not and value other than boolen
+alert( !!1 ); */
+
+
