09050b45e9ca484a7b0c18cf4fbeaae79554fd1e
Vusqa1003 <vusqarafique@gmail.com>
2021-04-08 12:08:16 +0500

    session 7

---
+++ session 7/index.js
@@ -0,0 +1,88 @@
+/*console.log("Hello Developers");
+console.log('Hello Developers');
+console.log('Hello \nDevelopers');
+
+var str1 = "Vusqa";
+console.log(str1);
+
+var str2= String('Rafique');
+console.log(str2);
+
+var str1 = "Hi";
+console.log(str1); // logs "Hi"
+console.log(str1.valueOf()); // logs "Hi"
+
+// template literals
+var myName = "Vusqa" ;
+var hello = "Hello "+ myName ;
+console.log(hello); 
+
+var template = `Hello from "template literals" article , check previous one 'arrow
+functions'.` ;
+console.log(template)
+
+const longString = `Lorem ipsum, dolor sit amet consectetur adipisicing elit.Excepturi laboriosam minus laudantium. Illum rem cupiditate quisquam,
+modi optio dolorem veritatis aliquam eius nam soluta.
+Dolorem adipisci natus voluptas laborum dolorum?`
+console.log(longString);*/
+
+
+
+// Comments
+// single line comment
+/* There are two ways to comment 
+single line 
+multiple line*/
+
+/*
+//Global scope
+var name= 'Hadia'; //global scope bcz declared outsidethe function 
+function func1() {
+    console.log(name); 
+     }
+    func1(); 
+
+    //local scope
+function f2(){
+    var lname='Rafique'; //lname is in local scope bcz declared inside function
+    console.log(lname);
+}
+f2();
+//console.log(lname); //if lname which is in local scope is accessed outside function then it will generate error
+
+// example of global and local combine
+var age = 100; //global scope
+function go(){
+var age = 200;  //local scope
+var hair = 'black'; // local scope
+console.log(age);
+console.log(hair);
+}
+go();
+console.log(age); // global is accessed here
+
+//scope- block statement
+if (true) {
+    // this 'if' conditional block doesn't create a new scope
+    var userName = 'John'; // userName is still in the global scope
+    }
+    console.log(userName); // output ‘John'
+*/
+
+
+    //other way to declare variable "let" "const"
+    var fname='Vusqa';
+    let age=21;
+    const height= 5.2;
+    console.log(name);
+    console.log(age);
+    console.log(height);
+
+
+// let and const create local scope 
+
+
+
+
+
+
