cf1c54e065bde119d3b8edcfe7c76f03c7c21286
Vusqa1003 <vusqarafique@gmail.com>
2021-04-09 14:10:02 +0500

    task 2

---
+++ task 2/index.js
@@ -0,0 +1,35 @@
+
+//-------Task 1------
+/*var str1;
+str1= `The self-study lessons in this section are written and organised
+according to the levels of the Common European Framework of Reference for languages (CEFR). 
+There are different types of texts and interactive exercises that practise the reading skills you need to do well in your studies, to get ahead at work and to communicate in English in your free time.`
+ console.log(str1);*/
+
+
+ //-------task2-------
+ /*var userName='Vusqa Rafique';
+ var date='9 April 2021';
+ var coverLetter=`${userName} Applicant
+ street no 5 house no 3 
+ Rehmanpura Sheikhupura
+ vusqarafique@gmail.com
+ ${date}
+ 
+ ${userName}
+ Dear Hiring Manager,
+
+ I'm excited to be applying for the Account Manager position at Cloud Clearwater. With more than seven years of experience in digital marketing agencies, 
+ I have developed a strong talent for building trusted relationships with high-value, high-impact clients.
+ During my previous role at River Tech, I managed a portfolio of more than a dozen clients in the healthcare and consumer goods industries.
+ I strategically coordinated a variety of DTC digital marketing campaigns that consistently delivered results, including over 20 campaigns that averaged a 10% or greater conversion rate. In addition to producing substantial ROI,
+ these campaigns also earned our top pharmaceutical clients multiple prestigious MM&M consumer awards in 2014, 2016, and 2017.
+ In addition to my experience working with existing client accounts, I was also given the opportunity to assist the business development team with RFPs for new clients in our healthcare vertical. I was responsible for selecting the pitch team, developing our presentation strategy, and spearheading post-pitch follow-up efforts. My efforts earned a 40% win rate, directly resulted in more than $15 million in new accounts, and landed projects with three of our CEO's key strategic account targets.
+ 
+ Thank you for your time and consideration. I'm looking forward to learning more details about the Account Manager position and about Cloud Clearwater. With a proven track record of account success, I am positioned to exceed your expectations for this role and substantially benefit your organization. I'm eager for the opportunity to share more examples of my work and discuss how my talents can be used to benefit Cloud Clearwater's client portfolio.
+ 
+ Sincerely,
+ 
+ ${userName};`
+ console.log(coverLetter);
+ */
