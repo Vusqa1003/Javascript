23cdd6b27993323dba64b4dbf33aeb1190582c67
Vusqa1003 <vusqarafique@gmail.com>
2021-04-15 01:45:43 +0500

    session 9

---
+++ session 9/index.js
@@ -0,0 +1,28 @@
+// console.log(document);
+// console.log(document.all);
+// console.log(document.all.length);
+// console.log(document.head.style);
+
+// console.log(document.URL);
+
+//console.log(document.title);
+
+ //const a = document.getElementById('ses');
+ //console.log(a.nodeName);
+
+//var b= document.getElementsByTagName('p')
+//console.log(b);
+
+var c= document.querySelector('p');
+console.log(c);
+
+var d= document.querySelectorAll('p')
+console.log(d);
+
+var allEle = document.querySelectorAll('.item');
+console.log(allEle);
+
+
+
+
+
